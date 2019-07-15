edad388a095eb9865768fe534b5ec0e96d6e0138
kellel14 <kellel14@gmail.com>
2019-07-15 14:40:54 -0400

    first commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private long ssn;
+    private int birthDate;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(long ssn) {
+        this.ssn = ssn;
+    }
+
+    public int getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(int birthDate) {
+        this.birthDate = birthDate;
+    }
+}
